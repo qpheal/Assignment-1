096f6b3922c58093cf57edf0e7e536b2779d16bc
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-16 12:25:18 +0000

    First Commit

---
+++ Assignment_1.js
@@ -0,0 +1,12 @@
+//Opearation taking addition as input
+function operation(firstNumber, secondNumber, operator){
+    if (operator == "+"){
+        return firstNumber + secondNumber
+    }   
+}
+
+
+const result = operation(2,3,"+")
+console.log(result)
+
+
