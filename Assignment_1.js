04937f03944a3bdb39181709a30d13662c939188
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-16 12:30:21 +0000

    Second Commit

--- Assignment_1.js
+++ Assignment_1.js
@@ -1,12 +1,14 @@
 //Opearation taking addition as input
-function operation(firstNumber, secondNumber, operator){
-    if (operator == "+"){
-        return firstNumber + secondNumber
-    }   
+function operation(firstNumber, secondNumber, operator) {
+  if (operator == "+") {
+    return firstNumber + secondNumber;
+  } else if (operator == "-") {
+    return firstNumber - secondNumber;
+  }
 }
 
+const additionResult = operation(2, 3, "+");
+console.log(additionResult); // 5
 
-const result = operation(2,3,"+")
-console.log(result)
-
-
+const subtractionResult = operation(2, 3, "-");
+console.log(subtractionResult); // -1
