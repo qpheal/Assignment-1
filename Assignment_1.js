567889de96eeab06912e659856da24b04d1c2e7d
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-16 12:35:19 +0000

    Third Commit

--- Assignment_1.js
+++ Assignment_1.js
@@ -4,7 +4,12 @@ function operation(firstNumber, secondNumber, operator) {
     return firstNumber + secondNumber;
   } else if (operator == "-") {
     return firstNumber - secondNumber;
+  } else if (operator == "*") {
+    return firstNumber * secondNumber;
+  } else {
+    return firstNumber / secondNumber
   }
+
 }
 
 const additionResult = operation(2, 3, "+");
@@ -12,3 +17,10 @@ console.log(additionResult); // 5
 
 const subtractionResult = operation(2, 3, "-");
 console.log(subtractionResult); // -1
+
+const multiplicationResult = operation(2, 3, "*");
+console.log(multiplicationResult); // 6
+
+const divisionResult = operation(2, 3, "/");
+console.log(divisionResult); // 0.66666666666666666
+
